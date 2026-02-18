48f6ccfca25613096bf0f449ce4f3f25213d04d0
synaptic4u <synaptic4u@gmail.com>
2026-02-18 10:04:53 +0200

    reset

--- dockerphp7/src/html/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-
-echo '<h2 style="text-align: center;">synaptic_webPHP7</h2>';
-phpinfo();
--- dockerphp8/src/html/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo '<h2 style="text-align: center;">synaptic_webPHP8</h2>';
-phpinfo();
